5654ccb54b67d10148be13478804cd08e74b3e01
majitsima <majd.hamza@rwth-aachen.de>
2021-06-06 21:01:47 +0200

    Fixed decoder.py

--- Abgabe 3/decoder.py
+++ Abgabe 3/decoder.py
@@ -2,7 +2,7 @@
 
 """
 
-from tensorflow.keras import callbacks
+from tensorflow.keras import Model
 from batches import *
 from custom_model import Perplexity, WordLabelerModel
 import tensorflow as tf
@@ -11,10 +11,29 @@ import utility as ut
 
 
 def tester(sor_file, tar_file, val_src, val_tar, window=2):
+    """
+    Load and test the model
+    """
+    batch = Batch()
+
+    # store source and target file as list of words
+    src = ut.read_from_file(sor_file)
+    trg = ut.read_from_file(tar_file)
+    val_src = ut.read_from_file(val_src)
+    val_tar = ut.read_from_file(val_tar)
+    # get word mapping for both training files and index files
+    get_word_index(src, trg)
+    # Needed for dictionary size TODO Maybe?
+    source, target = get_word_index(val_src, val_tar)
+    batch = get_all_batches(source, target, window)
+
+    # test_model = WordLabelerModel()
     test_model = tf.keras.models.load_model(
         "training_1/train_model.epoch01-loss3.94.hdf5",
         custom_objects={"WordLabelerModel": WordLabelerModel, "perplexity": Perplexity},
+        compile=False,
     )
+    print(test_model.summary())
     test_model.compile(
         optimizer="adam",
         loss="categorical_crossentropy",
@@ -25,23 +44,12 @@ def tester(sor_file, tar_file, val_src, val_tar, window=2):
         ],
     )
 
-    batch = Batch()
-    # BUG: REQ das Label muss während das lernen immer bekannt sein. S9 Architektur in letzte VL
-
-    # store source and target file as list of words
-    src = ut.read_from_file(sor_file)
-    trg = ut.read_from_file(tar_file)
-    # get word mapping for both training files and index files
-    source, target = get_word_index(src, trg)
-
-    # Needed for dictionary size TODO Maybe?
-    get_word_index(val_src, val_tar)
-    batch = get_all_batches(source, target, window)
-
     # predictions data preprocessing:
     feed_src = np.array(batch.source)
-    feed_src = {"I0": feed_src}
-    feed_src = tf.data.Dataset.from_tensor_slices(feed_src)
+    feed_src = {"I0": feed_src, "I1": np.array(batch.target)}
+    feed_src = tf.data.Dataset.from_tensor_slices(feed_src).batch(
+        200, drop_remainder=True
+    )
 
     # prediction step
     history = test_model.predict(feed_src, batch_size=200, callbacks=None)
