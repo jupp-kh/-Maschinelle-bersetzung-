dbc5173b269bc75478163a1e3dfa785430132ca0
majitsima <majd.hamza@rwth-aachen.de>
2021-07-05 22:27:25 +0200

    changed gitlab-ci.yml

--- compile_to_dicord.sh
+++ compile_to_dicord.sh
@@ -1,34 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
 CI_COMMIT_SHA="$1"
+## discord webhook
 DISCORD_WEBHOOK="$2"
 
 
-echo mkdir out
-# mkdir out
+data_msg="Report: All changes in repository \nDate: $(date)\n"
 
-for f in $(git diff-tree --no-commit-id --name-status -r $CI_COMMIT_SHA)
+
+output=($(git diff-tree --no-commit-id --name-status $CI_COMMIT_SHA))
+LEN=${#output[*]}
+
+
+for ((i=0;i<LEN;i++));
 do
-    f="$(echo "$f" | sed 's/"//g')"
-    case $f in
-        *.py)
-            echo Changed python file $f
-            ;;
-        *)
-            echo "Changes in " $f
-    esac
+    x=$i%2
+    if [[ $x -eq 0 ]];
+    then 
+        f=${output[$i]}
+        f="$(echo "$f" | sed 's/"//g' | sed 's/ //g')"
+        # echo $f
+        i=$i+1
+        case $f in
+            A*)
+                data_msg+="- Added new file ${output[$i]} \n"
+                ;;
+            M*)
+                data_msg+="- Modified file: ${output[$i]} \n"
+        esac
+    fi
 done
-# cd out || exit 1
-# for o in *
-# do
-#     case "$o" in
-#         *.html)
-#             name="$(echo "$o" | sed "s/\.html//")"
-#             # echo wkhtmltopdf "$name".html "$name".pdf
-#             # wkhtmltopdf "$name".html "$name".pdf
-#             curl --form file=@"$name".html "$DISCORD_WEBHOOK"
-#             # curl --form file=@"$name".pdf  "$DISCORD_WEBHOOK"
-#             ;;
-#     esac
-# done
+
+curl -H "Content-Type: application/json" -X POST -d "{\"content\": \"${data_msg}\"}" $DISCORD_WEBHOOK
+
 echo "done deer"
